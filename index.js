233b5185f5ca59d0bcab82198eb85f13c822ce04
TimH1107 <tim11071992@gmail.com>
2020-02-28 09:53:13 -0800

    Real initial commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+var i=0;
+
+class Member {
+  constructor(name) {
+    this.name = name;
+  };
+  setHeight(height) {
+  	this.height = height;
+  }
+  setWeight(weight) {
+  	this.weight = weight;
+  }
+  calcBMI() {
+  	this.bmi = this.weight / (this.height*this.height);
+  }
+}
+
+app.use(express.static('rourou'))
+
+app.get('/', (req, res) => {
+	/*res.set('Content-Type', 'text/html');
+
+	let output ='';
+	output += '<img src="/roubaozi/a.jpeg" style="border:3px solid red; margin:50px;">';
+	output += '<a href="https://google.com/"><button type="button">Google</button></a>';
+	output += '';
+	output += '';*/
+
+	let myArray = [];
+
+	let tmp = new Member('Tim');
+	tmp.setWeight(70);
+	tmp.setHeight(186);
+	tmp.calcBMI();
+	myArray.push(tmp);
+
+	tmp = new Member('Mengyi');
+	tmp.setWeight(55);
+	tmp.setHeight(173);
+	tmp.calcBMI();
+	myArray.push(tmp);
+
+	tmp = new Member('Roubaozi');
+	tmp.setWeight(3);
+	myArray.push(tmp);
+
+	/*let family = {
+		"name": "HoppWang",
+		"country": "USA",
+		"members": [
+			{
+				"age":"27",
+				"fullname":"Tim-Jannick Hopp"
+			},
+			{
+				"age":"25",
+				"fullname":"Mengyi Wang"
+			},
+			{
+				"age":"3",
+				"fullname":"Roubaozi Wang"
+			},
+		]
+	};*/
+
+	res.status(200).json(myArray);
+});
+app.get('/add', (req, res) => {
+	res.set('Content-Type', 'text/html');
+	res.status(200).send('value of i: ' + ++i);
+});
+
+app.get('/subtract', (req, res) => {
+	res.set('Content-Type', 'text/html');
+	res.status(200).send('value of i: ' + --i);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ public/js/naviBar.js
@@ -0,0 +1,35 @@
+jQuery(function($){
+	$('a[href*="#"]').on('click', function(e) {
+ 		e.preventDefault();
+
+		let href = $(this).attr('href');
+		if ($(href).length>0) {
+			$('html,body').animate({
+				scrollTop: $(href).offset().top
+			}, 500, 'swing');
+		}
+	});
+
+
+
+	$(window).scroll(function(e) {
+		console.log($('html').scrollTop());
+
+		let windowScroll=$('html').scrollTop();
+
+		$('.allDiv > div').each(function() {
+			let containerTop=$(this).offset().top;
+			let containerBottom=containerTop + $(this).outerHeight(true);
+
+			let moveID = $(this).attr('id');
+			if(windowScroll>=containerTop&&windowScroll<containerBottom) {
+				//this container is active
+				$('a[href="#' + moveID + '"]').addClass('linkActive');
+			} else {
+				//this container is NOT active
+				$('a[href="#' + moveID + '"]').removeClass('linkActive');
+			}
+		});
+
+	});
+});
